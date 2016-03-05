cb798b2e4b9893f64d99cb8e99b1a77bbbbad9bb
Dominik Buhl <dominik.buhl@ergosign.de>
2016-03-05 17:07:16 +0100

    add specs stuff

--- src/scripts/standalone/spec/spec.js
+++ src/scripts/standalone/spec/spec.js
@@ -3,5 +3,9 @@
 
 (function(antetype){
 
+    var colors = antetype.commands.getAllColorsInProject();
+    var fonts = antetype.commands.getAllFontsInProject();
+
+    log(colors, fonts);
 
 })(ANTETYPE_JS_CORE_FACTORY.getInstance());
